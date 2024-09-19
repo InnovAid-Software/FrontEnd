f0aaa3470cd51e308004678da058d13ee39bf62b
yjacobo3573 <90855493+yjacobo3573@users.noreply.github.com>
2024-09-19 10:21:18 -0500

    updates

--- JavaScript/Script1.js
+++ JavaScript/Script1.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
