3a41820ed6c5ebc6db17d9f8cd31201d876425b5
yjacobo3573 <90855493+yjacobo3573@users.noreply.github.com>
2024-10-10 18:06:46 -0500

    edits

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -13,7 +13,34 @@ login.addEventListener("click", () => {
     formSection.classList.remove("form-section-move");
 });
 
+ 
 
 const url = 'https://innovaid.dev/api/login';
 
 
+
+document.getElementById('signupForm').addEventListener('submit', async function(e)
+{
+    e.preventDefault();
+
+    const email = document.getElementById('signupForm').addEventListener('submit')
+    const password =
+    
+}
+
+document.getElementById('loginForm').addEventListener('submit', async function (e)
+{
+    //When the user clicks the submit button, capture
+    //the form data and send it as a Post request to backend
+    //API using fectch. send user's details as JSON to the server
+    e.preventDefault(); 
+
+    //get values from the form
+
+
+    
+
+
+
+
+}
